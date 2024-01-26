9e64b6e41d7a01b8ad04c6c66a5ce39c7f71a6b3
David <david@MacBook-Pro-de-David.local>
2024-01-26 14:14:34 -0300

    :sparkles: feat: home page component and styles

---
+++ frontend/src/pages/Home/Index.tsx
@@ -0,0 +1,12 @@
+import HelloContainer from "./components/HelloContainer";
+import OrangeContainer from "./components/OrangeContainer";
+
+
+export default function Home() {
+  return (
+    <div>
+        <HelloContainer/>
+        <OrangeContainer/>
+    </div>
+  )
+}
---
+++ frontend/src/pages/Home/Styles.ts
@@ -0,0 +1,66 @@
+import styled from "styled-components";
+
+
+
+export const TitleOrange = styled.h1`
+    font-size: 64px;
+    font-weight: 700;
+    font-family: Inter;
+    color: #D96C0D;
+`
+export const TextHello = styled.p`
+    align-items: center;
+    color: #027333;
+    font-family: Inter;
+    font-size: 20px;
+    font-style: normal;
+    font-weight: 400;
+    display: flex;
+`
+export const OrangeContainer = styled.div`
+    width: 100%;
+    height: 100vh;
+    border-radius: 41px 41px 0px 0px;
+    background-color: #FFB878;
+`
+export const ContainerLogin = styled.div`
+    border-radius: 260.5px 260.5px 0px 0px;
+    width: 521px;
+    height: 702px;
+    display:flex;
+    background-color: #fff;
+`
+export const FormArea = styled.form`
+    width: 100%;
+    margin-top: 150px;
+    padding: 0px 20px 0px 20px;
+
+`
+
+export const InputLogin = styled.input`
+    border-radius: 5px;
+    background: #D9D9D9;
+    height: 40px;
+    width: 100%;
+    padding-left: 10px;
+    margin-bottom: 10px;
+`
+export const TitleForm = styled.h1`
+    font-size: 64px;
+    font-weight: 700;
+    font-family: Inter;
+    color: #027333;
+    text-align: center;
+    margin-bottom: 10px
+`
+export const ButtonLogin = styled.button`
+    border-radius: 10px;
+    background: #027333;
+    width: 151px;
+    height: 45px;
+    font-weight: 500;
+    font-size: 20px;
+    text-align: center;
+    color: #fff;
+    margin-top: 10px
+`
