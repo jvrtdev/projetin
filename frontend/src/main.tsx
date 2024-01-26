759d44b04fc598e750901d04891ac7b755917b6d
David <david@MacBook-Pro-de-David.local>
2024-01-26 14:13:28 -0300

    :bug: fix: add route home

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
 import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
+import Home from './pages/Home/Index.tsx';
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <App />
+    element: <Home />
   }
 ])
 
