18002489e52303fc8e7c80643e8b8719a1d6dbcc
David <david@MacBook-Pro-de-David.local>
2024-01-29 19:19:40 -0300

    :lipstick: feat: add login and cadastro styles

---
+++ frontend/src/pages/Home/components/Cadastro.tsx
@@ -0,0 +1,66 @@
+import * as S from "../Styles.ts"
+import axios from "axios";
+//react hook form
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+//yup
+import { object, string } from "yup";
+
+interface SchemaType{
+    email: string;
+    password:string;
+
+}
+
+const schema = object<SchemaType>({
+    email: string()
+    .required("Campo obrigatório")
+    .min(3, "Você precisa inserir no minimo 3 caracteres"), 
+    password: string()
+    .required("Campo obrigatório")
+    .min(6, "Você precisa inserir no minimo 6 caracteres")
+    .max(10, "Sua senha não pode ter mais que 10 caracteres")
+})
+
+export default function Cadastro() {
+   
+    interface FormInput{
+        email: string;
+        password: string;
+    }
+    const { register, handleSubmit: onSubmit, watch, formState: {errors} } = useForm<FormInput>({ resolver: yupResolver(schema)})
+
+    const submitApi = async (data: any) => {
+        try{
+            const response = await axios.post('http://localhost:3000/user', data);
+            console.log("Resposta da API", response.data)
+
+        }
+        catch(error){
+            console.error('Deu ruim menor', error)
+        }
+    }
+
+    return (
+        <S.FormArea onSubmit={onSubmit(submitApi)}>
+            <S.TitleForm>Cadastro</S.TitleForm>
+
+            <S.TextHello>Email</S.TextHello>
+            
+            <S.InputLogin 
+            type="email"
+            {...register("email")}
+            />
+            <S.Validation>{errors?.email?.message}</S.Validation>
+            <S.TextHello>Senha</S.TextHello>
+            
+            <S.InputLogin 
+            type="password"
+            {...register("password")}
+            />
+            <S.Validation>{errors?.password?.message}</S.Validation>
+
+            <S.ButtonLogin type="submit">Entrar</S.ButtonLogin>
+        </S.FormArea>
+    )
+}
