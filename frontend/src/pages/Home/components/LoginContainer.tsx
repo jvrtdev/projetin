7635774a6f124d31fb39531eff4a695fa83cd5cd
David <david@MacBook-Pro-de-David.local>
2024-01-29 19:27:11 -0300

    :lipstick: feat: button login or sign up

--- frontend/src/pages/Home/components/LoginContainer.tsx
+++ frontend/src/pages/Home/components/LoginContainer.tsx
@@ -1,14 +1,47 @@
+import { useState } from "react"
 import * as S from "../Styles.ts"
-import Form from "./Form.tsx"
+import Cadastro from "./Cadastro.tsx"
+import Login from "./Login.tsx"
 
 
 export default function LoginContainer() {
+  const [loginOrSign, setLoginOrSign ] = useState<boolean>(false)
+
+  function SetLogin() {
+    setLoginOrSign(true)
+  }
+  function SetSign() {
+    setLoginOrSign(false)
+  }
+
   return (
-    <div className="flex justify-center pt-10">
+    <div className="flex justify-center pt-10 flex-col items-center">
+          {/* <S.ButtonSwitch onClick={() => {
+            if(loginOrSign == false ){
+              setLoginOrSign(true)
+            }else{
+              setLoginOrSign(false)
+            }
+          }}>
+            {loginOrSign == false ? "Login" : "Cadastro"}
+          </S.ButtonSwitch> */}
+          <S.SwitchContainer>
+            <S.SwitchButtonLogin isFalse={loginOrSign}
+            onClick={SetLogin}>
+              Login
+            </S.SwitchButtonLogin>
+
+            <S.SwitchButtonSign isFalse={loginOrSign}
+            onClick={SetSign}
+            >
+              Cadastro
+            </S.SwitchButtonSign>
+          </S.SwitchContainer>
         <S.ContainerLogin>
-          <Form />
-           
+          {loginOrSign == false ? <Cadastro /> : <Login /> }
+          
         </S.ContainerLogin>
+        
     </div>
   )
 }
