28d94df7668a24eda47911ec89bd5eb1ba5990f2
Miguel Luis <miguelluisataideferreira68@gmail.com>
2024-03-07 21:15:31 -0300

    feat: :lipstick: hover-color

--- frontend/src/pages/Home/components/Header/Header.tsx
+++ frontend/src/pages/Home/components/Header/Header.tsx
@@ -14,27 +14,27 @@ const Header = () => {
        className="bg-gray-200 text-black p-2 rounded-full w-[30%] placeholder:text-black"
      />
      <div className="flex items-center space-x-5 m-5">
-       <Link to=""  className="hover:scale-105 transition-transform duration-300 ease-in-out transform origin-center" >
+       <Link to=""  className="hover:scale-105 transition-transform duration-300 ease-in-out transform origin-center hover:text-verde" >
          <FaHome className="text-2xl ml-2"  />
          <span className="text-sm">Home</span>
        </Link>
-       <Link to=""  className="mr-2 hover:scale-105 transition-transform duration-300 ease-in-out transform origin-center" >
+       <Link to=""  className="mr-2 hover:scale-105 transition-transform duration-300 ease-in-out transform origin-center hover:text-verde" >
          <TiMessages className="text-2xl ml-6" />
          <span className="text-sm mr-2">Mensagens</span>
        </Link>
-       <Link to=""  className="hover:scale-105 transition-transform duration-300 ease-in-out transform origin-center" >
+       <Link to=""  className="hover:scale-105 transition-transform duration-300 ease-in-out transform origin-center hover:text-verde" >
          <FaVideo className="text-2xl ml-2" />
          <span className="text-sm">Mídia</span>
        </Link>
-       <Link to=""  className="hover:scale-105 transition-transform duration-300 ease-in-out transform origin-center" >
+       <Link to=""  className="hover:scale-105 transition-transform duration-300 ease-in-out transform origin-center hover:text-verde" >
          <FaBell className="text-2xl ml-6" />
          <span className="text-sm">Notificações</span>
        </Link>
-       <Link to=""  className="hover:scale-105 transition-transform duration-300 ease-in-out transform origin-center" >
+       <Link to=""  className="hover:scale-105 transition-transform duration-300 ease-in-out transform origin-center hover:text-verde" >
          <FaPaw className="text-2xl ml-2" />
          <span className="text-sm">Adoção</span>
        </Link>
-       <Link to="" className="hover:scale-105 transition-transform duration-300 ease-in-out transform origin-center" >
+       <Link to="" className="hover:scale-105 transition-transform duration-300 ease-in-out transform origin-center hover:text-verde" >
          <FaCircleUser  className="text-2xl ml-1" />
          <span className="text-sm">Perfil</span>
        </Link>
